b3e4cd148b740494ab6a564b24a0a15817f28abf
neumanf <fabricionewman@protonmail.com>
2023-01-20 08:49:29 -0300

    fix: add domain property to cookie

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,4 +1,5 @@
 export default () => ({
+    nodeEnv: process.env.NODE_ENV,
     frontendUrl: process.env.FRONTEND_URL,
     port: parseInt(process.env.PORT, 10) || 3000,
     databaseUrl: process.env.DATABASE_URL,
