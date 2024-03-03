bbccfeed448322ea4d7bf10029bf31a3b365b0c1
neumanf <fabricionewman@protonmail.com>
2024-03-02 21:19:27 -0300

    fix(ui): fix production config

--- apps/ui/src/main.ts
+++ apps/ui/src/main.ts
@@ -1,5 +1,11 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environment/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+    enableProdMode();
+}
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
