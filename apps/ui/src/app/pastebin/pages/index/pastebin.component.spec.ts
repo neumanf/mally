e1681de79e8b392585b9ca93ec9f235b60ee711b
neumanf <fabricionewman@protonmail.com>
2024-03-03 18:52:12 -0300

    chore: improve README, format files and add CI for formatting

--- apps/ui/src/app/pastebin/pages/index/pastebin.component.spec.ts
+++ apps/ui/src/app/pastebin/pages/index/pastebin.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PastebinComponent } from './pastebin.component';
 
 describe('PastebinComponent', () => {
-  let component: PastebinComponent;
-  let fixture: ComponentFixture<PastebinComponent>;
+    let component: PastebinComponent;
+    let fixture: ComponentFixture<PastebinComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PastebinComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PastebinComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [PastebinComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(PastebinComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- apps/ui/src/app/pastebin/pages/paste/paste.component.spec.ts
+++ apps/ui/src/app/pastebin/pages/paste/paste.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PasteComponent } from './paste.component';
 
 describe('PasteComponent', () => {
-  let component: PasteComponent;
-  let fixture: ComponentFixture<PasteComponent>;
+    let component: PasteComponent;
+    let fixture: ComponentFixture<PasteComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PasteComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PasteComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [PasteComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(PasteComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
