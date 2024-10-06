c05e263d93701ad24870b7e18df9fb7b8dad8072
neumanf <fabricionewman@protonmail.com>
2024-10-06 11:01:31 -0300

    style: add a splash screen

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import {
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
+} from '@angular/animations';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
+    animations: [
+        trigger('fadeIn', [
+            state('void', style({ opacity: 0 })), // Initial state: hidden
+            transition(':enter', [
+                animate('1s ease-in', style({ opacity: 1 })), // Animate to visible state
+            ]),
+        ]),
+        trigger('fadeOut', [
+            state('void', style({ opacity: 1 })), // Initial state: visible
+            transition(':leave', [
+                animate('0.25s ease-out', style({ opacity: 0 })), // Fade-out animation
+            ]),
+        ]),
+    ],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     title = 'ui';
+    splashVisible = true;
+
+    ngAfterViewInit() {
+        setTimeout(() => {
+            this.splashVisible = false; // Trigger fade-out after timeout
+        }, 250);
+    }
 }
